22339272836eede991045848a0d45b1e40c7147d
Junayed Hossain <158603605+junayed233@users.noreply.github.com>
2026-09-12 11:48:49 +0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -115,7 +115,6 @@ def get_quests():
 def main():
 
     seen = load_seen()
-    send_telegram("✅ MineBit monitor is working!")
     quests = get_quests()
 
     print(f"Found {len(quests)} quests.")
